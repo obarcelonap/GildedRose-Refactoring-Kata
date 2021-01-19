5423c7e6a07e9df0b76d8895d902122d2de55d0c
Oriol Barcelona <obarcelonap@gmail.com>
2021-01-19 13:57:39 +0100

    Isolate aged brie case

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -10,7 +10,16 @@ class GildedRose(var items: Array<Item>) {
                 break
             }
 
-            if (item.quality < 50 && (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name == "Aged Brie") {
+                item.quality = minOf(item.quality + 1, 50)
+                item.sellIn = item.sellIn - 1
+                if (item.sellIn < 0) {
+                    item.quality = minOf(item.quality + 1, 50)
+                }
+                break
+            }
+
+            if (item.quality < 50 && (item.name == "Backstage passes to a TAFKAL80ETC concert")) {
                 item.quality = item.quality + 1
 
                 if (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.quality < 50) {
@@ -28,9 +37,7 @@ class GildedRose(var items: Array<Item>) {
             item.sellIn = item.sellIn - 1
 
             if (item.sellIn < 0) {
-                if (item.name == "Aged Brie" && item.quality < 50) {
-                    item.quality = item.quality + 1
-                } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                     item.quality = 0
                 } else if (item.quality > 0) {
                     item.quality = item.quality - 1
