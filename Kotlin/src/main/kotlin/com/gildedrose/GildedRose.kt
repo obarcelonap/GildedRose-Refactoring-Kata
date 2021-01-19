e716fc6fdbbfdc575dc761606f64396e2ea90289
Oriol Barcelona <obarcelonap@gmail.com>
2021-01-19 14:01:52 +0100

    Isolate regular item case:

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -39,11 +39,8 @@ class GildedRose(var items: Array<Item>) {
                 break
             }
 
-
-            if (item.quality > 0) {
-                item.quality = item.quality - 1
-            }
-
+            // Regular item
+            item.quality = maxOf(item.quality - 1, 0)
             item.sellIn = item.sellIn - 1
 
             if (item.sellIn < 0 && item.quality > 0) {
