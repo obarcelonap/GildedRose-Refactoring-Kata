c5243748bf48acff03e4c7d27626df6636cbfa6b
Oriol Barcelona <obarcelonap@gmail.com>
2021-01-19 15:11:50 +0100

    Embrace immutability

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,52 +1,54 @@
 package com.gildedrose
 
-class GildedRose(var items: Array<Item>) {
+class GildedRose(val items: Array<Item>) {
 
-    fun updateQuality() {
-        for (item in items) {
-            if (item.name == "Sulfuras, Hand of Ragnaros") {
-                item.quality = 80
-                item.sellIn = 0
-                break
-            }
+    fun updateQuality(): GildedRose {
+        val newItems = items.map { updateItem(it) }
+            .toTypedArray()
 
-            if (item.name == "Aged Brie") {
-                item.quality = minOf(item.quality + 1, 50)
-                item.sellIn = item.sellIn - 1
-                if (item.sellIn < 0) {
-                    item.quality = minOf(item.quality + 1, 50)
-                }
-                break
-            }
+        return GildedRose(newItems)
+    }
 
-            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                item.quality = item.quality + 1
-
-                if (item.quality < 50) {
-                    if (item.sellIn < 11) {
-                        item.quality = item.quality + 1
-                    }
-                    if (item.sellIn < 6) {
-                        item.quality = item.quality + 1
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1
-
-                if (item.sellIn < 0) {
-                    item.quality = 0
-                }
-                break
-            }
-
-            // Regular item
-            item.quality = maxOf(item.quality - 1, 0)
-            item.sellIn = item.sellIn - 1
-
-            if (item.sellIn < 0 && item.quality > 0) {
-                item.quality = item.quality - 1
-            }
+    private fun updateItem(item: Item): Item {
+        if (item.name == "Sulfuras, Hand of Ragnaros") {
+            return Item("Sulfuras, Hand of Ragnaros", 0, 80)
         }
+
+        if (item.name == "Aged Brie") {
+            val sellIn = item.sellIn - 1
+
+        var quality = minOf(item.quality + 1, 50)
+        if (sellIn < 0) {
+            quality = minOf(quality + 1, 50)
+        }
+        return Item(item.name, sellIn, quality)
+    }
+
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            val sellIn = item.sellIn - 1
+
+            var quality = minOf(item.quality + 1, 50)
+            if (sellIn < 11) {
+                quality = minOf(quality + 1, 50)
+            }
+            if (sellIn < 6) {
+                quality = minOf(quality + 1, 50)
+            }
+
+        if (sellIn < 0) {
+            quality = 0
+        }
+        return Item(item.name, sellIn, quality)
+    }
+
+        // Regular item
+        var quality = maxOf(item.quality - 1, 0)
+        val sellIn = item.sellIn - 1
+
+        if (sellIn < 0 && quality > 0) {
+            quality = maxOf(quality - 1, 0)
+        }
+        return Item(item.name, sellIn, quality)
     }
 
 }
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -9,9 +9,9 @@ internal class GildedRoseTest {
     fun `lower by 1 sellIn and quality on update`() {
         val items = arrayOf(Item("foo", 5, 5))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(4, sellIn)
             assertEquals(4, quality)
         }
@@ -21,9 +21,9 @@ internal class GildedRoseTest {
     fun `lower by 2 quality when sellIn is less than 0`() {
         val items = arrayOf(Item("foo", -1, 50))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(48, quality)
         }
     }
@@ -32,9 +32,9 @@ internal class GildedRoseTest {
     fun `quality is never negative`() {
         val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(0, quality)
         }
     }
@@ -43,9 +43,9 @@ internal class GildedRoseTest {
     fun `"Aged Brie" increases in quality the older it gets`() {
         val items = arrayOf(Item("Aged Brie", 0, 0))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(2, quality)
         }
     }
@@ -54,9 +54,9 @@ internal class GildedRoseTest {
     fun `quality is never more than 50`() {
         val items = arrayOf(Item("Aged Brie", 0, 50))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(50, quality)
         }
     }
@@ -65,9 +65,9 @@ internal class GildedRoseTest {
     fun `"Sulfuras" never has to be sold or decreases in quality`() {
         val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 0, 80))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(0, sellIn)
             assertEquals(80, quality)
         }
@@ -77,9 +77,9 @@ internal class GildedRoseTest {
     fun `"Backstage passes" increases quality by 2 when there are 10 days or less`() {
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 0))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(2, quality)
         }
     }
@@ -88,9 +88,9 @@ internal class GildedRoseTest {
     fun `"Backstage passes" increases quality by 3 when there are 5 days or less`() {
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 0))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(3, quality)
         }
     }
@@ -99,9 +99,9 @@ internal class GildedRoseTest {
     fun `"Backstage passes" quality drops to 0 after the concert`() {
         val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 5))
         val app = GildedRose(items)
-        app.updateQuality()
+        val updatedApp = app.updateQuality()
 
-        with(app.items.first()) {
+        with(updatedApp.items.first()) {
             assertEquals(0, quality)
         }
     }
