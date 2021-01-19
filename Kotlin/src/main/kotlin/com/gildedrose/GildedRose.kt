6cad24251824f460dae2686c0c4cae9011e3dee2
Oriol Barcelona <obarcelonap@gmail.com>
2021-01-19 19:30:45 +0100

    Move sellIn decrease to GildedRose

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,13 +9,17 @@ class GildedRose(val items: Array<Item>) {
         return GildedRose(newItems)
     }
 
-    private fun updateItem(item: Item): Item = when (item) {
-        is Sulfuras -> item.updateQuality()
-        is AgedBrie -> item.updateQuality()
-        is BackstagePasses -> item.updateQuality()
-        is Regular -> item.updateQuality()
-        is Conjured -> item.updateQuality()
-        else -> throw NotImplementedError("Missing implementation for item $item")
+    private fun updateItem(item: Item): Item {
+        val sellIn = item.sellIn - 1
+
+        return when (item) {
+            is Sulfuras -> item.updateQuality(sellIn)
+            is AgedBrie -> item.updateQuality(sellIn)
+            is BackstagePasses -> item.updateQuality(sellIn)
+            is Regular -> item.updateQuality(sellIn)
+            is Conjured -> item.updateQuality(sellIn)
+            else -> throw NotImplementedError("Missing implementation for item $item")
+        }
     }
 }
 
--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -7,9 +7,8 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
 }
 
 class Regular(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
-    fun updateQuality(): Regular {
+    fun updateQuality(sellIn: Int): Regular {
         var quality = maxOf(quality - 1, 0)
-        val sellIn = sellIn - 1
 
         if (sellIn < 0 && quality > 0) {
             quality = maxOf(quality - 1, 0)
@@ -19,13 +18,11 @@ class Regular(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, qual
 }
 
 object Sulfuras : Item("Sulfuras, Hand of Ragnaros", 0, 80) {
-    fun updateQuality(): Sulfuras = this
+    fun updateQuality(sellIn: Int): Sulfuras = this
 }
 
 class AgedBrie(sellIn: Int, quality: Int) : Item("Aged Brie", sellIn, quality) {
-    fun updateQuality(): AgedBrie {
-        val sellIn = sellIn - 1
-
+    fun updateQuality(sellIn: Int): AgedBrie {
         var quality = minOf(quality + 1, 50)
         if (sellIn < 0) {
             quality = minOf(quality + 1, 50)
@@ -35,9 +32,7 @@ class AgedBrie(sellIn: Int, quality: Int) : Item("Aged Brie", sellIn, quality) {
 }
 
 class BackstagePasses(sellIn: Int, quality: Int) : Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) {
-    fun updateQuality(): BackstagePasses {
-        val sellIn = sellIn - 1
-
+    fun updateQuality(sellIn: Int): BackstagePasses {
         var quality = minOf(quality + 1, 50)
         if (sellIn < 11) {
             quality = minOf(quality + 1, 50)
@@ -54,13 +49,12 @@ class BackstagePasses(sellIn: Int, quality: Int) : Item("Backstage passes to a T
 }
 
 class Conjured(sellIn: Int, quality: Int): Item("Conjured", sellIn, quality) {
-    fun updateQuality(): Regular {
+    fun updateQuality(sellIn: Int): Conjured {
         var quality = maxOf(quality - 2, 0)
-        val sellIn = sellIn - 1
 
         if (sellIn < 0 && quality > 0) {
             quality = maxOf(quality - 2, 0)
         }
-        return Regular(name, sellIn, quality)
+        return Conjured(sellIn, quality)
     }
 }
